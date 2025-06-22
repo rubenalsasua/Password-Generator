92ba4de6ce67fff4e2e102b6acc0ce7ba0bd54af
rubenalsasua <r.alsasua@opendeusto.es>
2025-06-22 12:41:47 +0200

    Pruebas unitarias con unittest

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import unittest
+import string
+from App.views import generar_password
 
-# Create your tests here.
+
+class TestPasswordGenerator(unittest.TestCase):
+    def test_password_length(self):
+        min_length = 8
+        max_length = 12
+        password = generar_password(min_length, max_length, True, True)
+        self.assertTrue(min_length <= len(password) <= max_length)
+
+    def test_password_alphabet(self):
+        min_length = 10
+        max_length = 10
+        password = generar_password(min_length, max_length, False, False)
+        for char in password:
+            self.assertIn(char, string.ascii_lowercase)
+
+
+if __name__ == "__main__":
+    unittest.main()
